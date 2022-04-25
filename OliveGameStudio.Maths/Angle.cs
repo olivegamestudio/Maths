78258a3cc55f8c474cd71ed3fa8c32a61e3d9a16
Simon Jefferies <simon@olivegame.studio>
2022-04-25 19:45:12 +0100

    Added initial classes/structs.

---
+++ OliveGameStudio.Maths/Angle.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OliveGameStudio;
+
+public readonly struct Angle
+{
+    readonly float _degrees;
+
+    public Angle(float degrees = 0)
+    {
+        _degrees = degrees;
+    }
+
+    public static Angle operator *(Angle source, float value)
+    {
+        return new Angle(source._degrees * value);
+    }
+
+    public static Angle operator +(Angle source, float value)
+    {
+        return new Angle(source._degrees + value);
+    }
+
+    public static Angle operator -(Angle source, float value)
+    {
+        return new Angle(source._degrees - value);
+    }
+
+    public static Angle operator -(Angle source)
+    {
+        return new Angle(-source._degrees);
+    }
+
+    public static Angle operator +(Angle source, Angle value)
+    {
+        return new Angle(source._degrees + value._degrees);
+    }
+
+    public static Angle operator -(Angle source, Angle value)
+    {
+        return new Angle(source._degrees - value._degrees);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public float ToDegrees()
+    {
+        return _degrees;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public float ToRadians()
+    {
+        return (float)Maths.ToRadians(_degrees);
+    }
+}
---
+++ OliveGameStudio.Maths/Maths.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OliveGameStudio;
+
+public static class Maths
+{
+    public static double ToDegrees(double radians)
+    {
+        return (180 / Math.PI) * radians;
+    }
+
+    public static double ToRadians(double degrees)
+    {
+        return (Math.PI / 180) * degrees;
+    }
+}
